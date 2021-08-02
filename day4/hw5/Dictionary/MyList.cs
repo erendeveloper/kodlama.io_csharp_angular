909bf7a41508d6fc15088b5a755bc2fa2ee75ee9
erendeveloper <48724906+erendeveloper@users.noreply.github.com>
2021-08-03 00:12:23 +0300

    day4 hw5
    day4 hw5

---
+++ day4/hw5/Dictionary/MyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyList<T>
+    {
+        T[] _myList;
+        public MyList()
+        {
+            _myList = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] _tempList = _myList;
+            _myList = new T[_myList.Length+1];
+
+            for(int i=0; i < _myList.Length-1; i++)
+            {
+                _myList[i] = _tempList[i];
+            }
+            _myList[_myList.Length - 1] = item;
+        }
+
+        public int Length()
+        {
+            return _myList.Length;
+        }
+    }
+}
---
+++ day4/hw5/Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+
+            Console.WriteLine(myList.Length());
+        
+        }
+    }
+}
