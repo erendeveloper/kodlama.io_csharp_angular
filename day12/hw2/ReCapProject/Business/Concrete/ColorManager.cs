d86ab600505cdde998691575d9fbe0cec06d052d
erendeveloper <48724906+erendeveloper@users.noreply.github.com>
2022-08-01 04:41:12 +0300

    day12 hw2
    day12 hw2

---
+++ day12/hw2/ReCapProject/Business/Concrete/ColorManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ColorManager : IColorService
+    {
+        IColorDal _colorDal;
+
+        public ColorManager(IColorDal colorDal)
+        {
+            _colorDal = colorDal;
+        }
+
+        public IResult Add(Color color)
+        {
+            _colorDal.Add(color);
+            return new SuccessResult(Messages.ItemAdded);
+        }
+
+        public IResult Delete(Color color)
+        {
+            _colorDal.Delete(color);
+            return new SuccessResult(Messages.ItemDeleted);
+        }
+
+        public IDataResult<List<Color>> GetAll()
+        {
+            return new SuccessDataResult<List<Color>>
+                (
+                _colorDal.GetAll()
+                );
+        }
+
+        public IDataResult<Color> GetById(int id)
+        {
+            return new SuccessDataResult<Color>
+                (
+                _colorDal.Get(p => p.Id == id)
+                ); 
+        }
+
+        public IResult Update(Color color)
+        {          
+            _colorDal.Update(color);
+            return new SuccessResult(Messages.ItemUpdated);
+        }
+    }
+}
